54d202c3eeab051bef93251610b22d67753164c0
eugene.bredyuk <eugene.bredyuk@gmail.com>
2017-06-02 23:31:34 +0300

    Added contact form

--- basic_website/suggest.php
+++ basic_website/suggest.php
@@ -5,7 +5,28 @@ $section = "suggest";
 include("inc/header.php"); ?>
 
 <div class="section page">
-    <h1>Suggest a Media Item</h1>
+    <div class="wrapper">
+        <h1>Suggest a Media Item</h1>
+        <p>Yo! Add some info here dog!</p>
+        <form method="post" action="post">
+
+            <table>
+                <tr>
+                    <th><label for="name">Name</label></th>
+                    <td><input id="name" type="text" name="name"></td>
+                </tr>
+                <tr>
+                    <th><label for="email">Email</label></th>
+                    <td><input id="email" type="email" name="email"></td>
+                </tr>
+                <tr>
+                    <th><label for="details">Details</label></th>
+                    <td><textarea id="details" name="details"></textarea></td>
+                </tr>
+            </table>
+            <input type="submit" value="Send">
+        </form>
+    </div>
 </div>
 
 <?php include("inc/footer.php"); ?>

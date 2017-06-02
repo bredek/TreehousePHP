1086df7f8cce033e674a095c2849ae66c3d08f4e
eugene.bredyuk <eugene.bredyuk@gmail.com>
2017-06-02 21:42:56 +0300

    Randomized index output

--- basic_website/inc/functions.php
+++ basic_website/inc/functions.php
@@ -1,10 +1,15 @@
 <?php 
-
+// setting active menu item
 function set_active_menu($section, $id){
-    
     if($section && $id && strtolower($section) == strtolower($id)){
         return " on";
     }
+}
+
+function generate_catalog($catalog){
+    foreach ($catalog as $item) {
+       echo "<li><a href='#'><img src=" . $item['img'] . " alt=" . $item['title'] . "><p>View info</p></a></li>";
+   }
 }
 
 ?>
--- basic_website/index.php
+++ basic_website/index.php
@@ -1,6 +1,9 @@
 <?php 
+// get data
+include("inc/data.php"); 
 $title = 'Index';
 include("inc/header.php"); 
+
 ?>
 		<div class="section catalog random">
 
@@ -8,11 +11,26 @@ include("inc/header.php");
 
 				<h2>May we suggest something?</h2>
 
+				<?php 
+					$random = array_rand($catalog, 4); 
+				?>
+					
 				<ul class="items">
-					<li><a href="details.php?id=201"><img src="img/media/forest_gump.jpg" alt="Forrest Gump"><p>View Details</p></a></li>
-					<li><a href="details.php?id=204"><img src="img/media/princess_bride.jpg" alt="The Princess Bride"><p>View Details</p></a></li>
-					<li><a href="details.php?id=302"><img src="img/media/elvis_presley.jpg" alt="Elvis Forever"><p>View Details</p></a></li>
-					<li><a href="details.php?id=303"><img src="img/media/garth_brooks.jpg" alt="No Fences"><p>View Details</p></a></li>
+
+					<?php foreach ($random as $id) : ?>
+
+						<li>
+							<a href="#">
+								<img 
+									src="<?php echo $catalog[$id]['img']; ?> " 
+									alt="<?php echo $catalog[$id]['title']; ?>"
+								>
+								<p>View info</p>
+							</a>
+						</li>
+
+					<?php endforeach; ?>
+
 				</ul>
 
 			</div>
